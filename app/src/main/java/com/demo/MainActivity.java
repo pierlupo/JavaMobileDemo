259e0908ba1bf18b7af3976e183a473e0c4fdcd9
pierlupo <garuda.kraimps@laposte.net>
2023-08-30 17:28:52 +0200

    wip

--- app/src/main/java/com/demo/MainActivity.java
+++ app/src/main/java/com/demo/MainActivity.java
@@ -1,10 +1,7 @@
 package com.demo;
 
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,34 +15,44 @@ public class MainActivity extends AppCompatActivity {
 
     private RadioButton radioButtonMale;
 
+    private int currentImageIndex = 0;
+
+    private int[] images = {R.drawable.chat, R.drawable.chat2_foreground, R.drawable.cat_foreground};
+
+    private ImageButton btnPrev;
+
+    private ImageButton btnNext;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_linear);
+        setContentView(R.layout.layout_frame);
 
-        btn = findViewById(R.id.btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                editTextName= findViewById(R.id.editTextName);
-
-                editTextEmail= findViewById(R.id.editTextEmail);
-
-                String name  = editTextName.getText().toString();
-                String email = editTextEmail.getText().toString();
-
-                radioButtonMale = findViewById(R.id.radioButtonMale);
-
-                String genre = radioButtonMale.isChecked() ?  "Homme" : "Femme";
-
-                String message = "Nom : " + name + " " + "email :" + email + " " + "Genre : " + genre ;
-
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
+//        btn = findViewById(R.id.btn);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                editTextName= findViewById(R.id.editTextName);
+//
+//                editTextEmail= findViewById(R.id.editTextEmail);
+//
+//                String name  = editTextName.getText().toString();
+//                String email = editTextEmail.getText().toString();
+//
+//                radioButtonMale = findViewById(R.id.radioButtonMale);
+//
+//                String genre = radioButtonMale.isChecked() ?  "Homme" : "Femme";
+//
+//                String message = "Nom : " + name + " " + "email :" + email + " " + "Genre : " + genre ;
+//
+//                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//    }
     }
 }
